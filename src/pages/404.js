4a01fe455f0565a9ff7140daf16694fa4bf34f9d
mobasergio <mobasergio@gmail.com>
2019-11-29 16:47:38 +0100

    404 page styles added

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,11 +3,20 @@ import { Link } from "gatsby"
 
 const NotFoundPage = () => {
   return (
-    <div id="notfound">
-      <h1>404</h1>
-      <h2>Page Not Found</h2>
-      <Link to="/">Go Home</Link>
-    </div>
+    <>
+      <Helmet>
+        <title>Sergio Moreno | Frontend web developer</title>
+        <meta name="description" content="Sergio Moreno is a frontend web developer based in Barcelona, Spain who specializes in developing (and occasionally designing) exceptional, high-quality websites and applications."/>  
+        <link rel="icon" href={Favicon} />
+      </Helmet>
+      <Layout>
+        <div id="notfound">
+          <h1>404</h1>
+          <h2>Page Not Found</h2>
+          <Link to="/">Go Home</Link>
+        </div>
+      </Layout>
+    </>
   );
 }
  
